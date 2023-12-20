33f7c12ade2e8b6b3152b2827f08b067c291f6b5
Syahravi <cc@syahravi.my.id>
2023-12-20 21:09:00 +0700

    v2

--- main.py
+++ main.py
@@ -1,26 +1,12 @@
-from fastapi import FastAPI, HTTPException, Depends
-from fastapi.responses import JSONResponse
+from fastapi import FastAPI, HTTPException, Form
 from pydantic import BaseModel
-from fastapi.security import OAuth2PasswordBearer
-import secrets
-import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import linear_kernel
-from typing import List
+import pandas as pd
 
 app = FastAPI()
 
-# Simpan kunci rahasia
-SECRET_KEY = "mysecretkey"
-
-# OAuth2PasswordBearer untuk otentikasi token
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
-
-# Fungsi untuk menghasilkan token
-def create_access_token(data: dict):
-    return secrets.token_urlsafe(32)
-
-# Load and preprocess dataset (assuming it's already loaded)
+# Load and preprocess dataset
 df = pd.read_csv("DatasetHerbal.csv")
 nama_herbal_column_name = 'Nama Herbal'
 khasiat_column_name = 'Khasiat'
@@ -43,7 +29,7 @@ df['combined_text'] = df[nama_herbal_column_name] + ' ' + df[khasiat_column_name
 tfidf_vectorizer = TfidfVectorizer(ngram_range=(1, 3), min_df=2, sublinear_tf=True)
 tfidf_matrix = tfidf_vectorizer.fit_transform(df['combined_text'])
 
-# Fungsi untuk merekomendasikan rempah berdasarkan input pengguna
+# Function to recommend rempah based on user input
 def recommend_rempah(input_text):
     input_text = input_text.lower()
     input_vector = tfidf_vectorizer.transform([input_text])
@@ -58,36 +44,17 @@ def recommend_rempah(input_text):
     # Sort and get top 7 recommendations
     related_rempah_indices = sorted(nonzero_indices, key=lambda i: cosine_similarities[i], reverse=True)[:7]
 
-    recommended_rempah = df.iloc[related_rempah_indices][['Nama Herbal', 'Khasiat', 'Saran Penyajian']]
+    recommended_rempah = df.iloc[related_rempah_indices][['original_case_nama_herbal', 'original_case_khasiat', 'original_case_saran']]
     return recommended_rempah.to_dict(orient='records')  # Return recommended rempah as a list of dictionaries
 
 class InputPayload(BaseModel):
     penyakit: str
 
-# Endpoint untuk mendapatkan token
-@app.post("/token")
-async def login_for_access_token(form_data: InputPayload):
-    if form_data.penyakit == "testuser" and form_data.penyakit == "testpassword":
-        access_token = create_access_token(data={"sub": form_data.penyakit})
-        return {"access_token": access_token, "token_type": "bearer"}
-    else:
-        raise HTTPException(
-            status_code=401,
-            detail="Invalid credentials",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
-
-# Endpoint terlindungi yang memerlukan token
-@app.post("/recommend", response_model=List[dict])
-async def get_recommendation(
-    payload: InputPayload, token: str = Depends(oauth2_scheme)
-):
+@app.post("/recommend", response_model=list)
+async def get_recommendation(payload: InputPayload):
     result = recommend_rempah(payload.penyakit)
 
     if result is not None:
         return result
     else:
-        raise HTTPException(
-            status_code=404,
-            detail=f'Tidak ada rekomendasi yang sesuai untuk penyakit {payload.penyakit}.'
-        )
+        raise HTTPException(status_code=404, detail=f'Tidak ada rekomendasi yang sesuai untuk penyakit {payload.penyakit}.')
